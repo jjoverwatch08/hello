a5fbb5f55617aa86c1d0a0a73355f94ea4179dae
Jacob Simpson <jsimpson35@huskers.unl.edu>
2026-08-27 11:30:18 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jacob Simpson
+ *         jsimpson35@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jacob Simpson\n");
+
+  return 0;
+}
