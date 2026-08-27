f37be76d5088ab94b8e0bc197a5d6f4ef94c42d4
bjahnke4 <bjahnke4@huskers.unl.edu>
2026-08-27 11:19:28 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ben Jahnke
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World!\n");
+    printf("I love the School of Computing!\n");
+    printf("My name is Ben J.\n");
+    return 0;
+}
+
