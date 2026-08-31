f0bf5000259c0c0bd54a916ed1b0151d1daacfd7
Jacob Simpson <jsimpson35@huskers.unl.edu>
2026-08-31 17:21:20 -0500

    added my name and major

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@
 int main(int argc, char **argv) {
     printf("Hello World, I'm Ben Jahnke!!\n");
     printf("My major is Computer Science!\n");
+    printf("My name is Jacob Simpson!\n");
+    prinf("My major is also Computer Science!\n");
     return 0;
 }
 
